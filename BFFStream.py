6fc3f2ebf6361a0b394b7208e0e7a38ca4e81e0f
Thomas Jackson Park <42450501+Mystified131@users.noreply.github.com>
2021-02-11 19:56:03 -0600

    Update BFFStream.py

--- BFFStream.py
+++ BFFStream.py
@@ -1,23 +1,12 @@
 #This code imports the necessary modules.
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 import random, datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mystified131:Jackson131!@mystified131.mysql.pythonanywhere-services.com/mystified131$APPSTotal'
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-db = SQLAlchemy(app)
 
 app.secret_key = 'noiiugubj3irg'
-
-class APPSTotal(db.Model):
-    sessiondata = db.Column(db.String(120), primary_key=True)
-
-    def __init__(self, sessiondata):
-        self.sessiondata = sessiondata
-
 
 @app.route('/', methods=['POST', 'GET'])
 def sessstart():
@@ -58,20 +47,6 @@ def make_player():
         atracknum = random.randrange(0,len(contentb))
         atrack = contentb[atracknum]
         tlst.append(atrack)
-
-
-    #dice = random.randrange(15)
-
-    #if dice > 14:
-
-        #atrack = ""
-
-        #for trk in range(18):
-
-            #tlst.append(atrack)
-
-
-        #tlst[18] =  "/static/GenWeatherSpot.mp3"
 
     return render_template('BFF.html', toplay1 = tlst[0], toplay2 = tlst[1], toplay3 = tlst[2], toplay4 = tlst[3], toplay5 = tlst[4], toplay6 = tlst[5], toplay7 = tlst[6], toplay8 = tlst[7], toplay9 = tlst[8], toplay10 = tlst[9], toplay11 = tlst[10], toplay12 = tlst[11], toplay13 = tlst[12], toplay14 = tlst[13], toplay15 = tlst[14],  toplay16 = tlst[15], toplay17 = tlst[16], toplay18 = tlst[17], toplay19 = tlst[18], toplay20 = tlst[19], toplay21 = tlst[20], toplay22 = tlst[21], toplay23 = tlst[22], songlen = songlength )
 
