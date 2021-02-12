4577602037613ad79f99d3a979edd0cefe3aacb4
Thomas Jackson Park <42450501+Mystified131@users.noreply.github.com>
2021-02-11 20:10:43 -0600

    Update BFFStream.py

--- BFFStream.py
+++ BFFStream.py
@@ -20,7 +20,7 @@ def sessstart():
     new_entry = APPSTotal(sessiondata)
     db.session.add(new_entry)
     db.session.commit()
-    return render_template('BFF.html')
+    return render_template('BFF2.html')
 
 #This code constructs the player page. It chooses an item randomly from a set of lists, and, after processing, opens a player page and cues up the item.
 
@@ -48,7 +48,7 @@ def make_player():
         atrack = contentb[atracknum]
         tlst.append(atrack)
 
-    return render_template('BFF.html', toplay1 = tlst[0], toplay2 = tlst[1], toplay3 = tlst[2], toplay4 = tlst[3], toplay5 = tlst[4], toplay6 = tlst[5], toplay7 = tlst[6], toplay8 = tlst[7], toplay9 = tlst[8], toplay10 = tlst[9], toplay11 = tlst[10], toplay12 = tlst[11], toplay13 = tlst[12], toplay14 = tlst[13], toplay15 = tlst[14],  toplay16 = tlst[15], toplay17 = tlst[16], toplay18 = tlst[17], toplay19 = tlst[18], toplay20 = tlst[19], toplay21 = tlst[20], toplay22 = tlst[21], toplay23 = tlst[22], songlen = songlength )
+    return render_template('BFF2.html', toplay1 = tlst[0], toplay2 = tlst[1], toplay3 = tlst[2], toplay4 = tlst[3], toplay5 = tlst[4], toplay6 = tlst[5], toplay7 = tlst[6], toplay8 = tlst[7], toplay9 = tlst[8], toplay10 = tlst[9], toplay11 = tlst[10], toplay12 = tlst[11], toplay13 = tlst[12], toplay14 = tlst[13], toplay15 = tlst[14],  toplay16 = tlst[15], toplay17 = tlst[16], toplay18 = tlst[17], toplay19 = tlst[18], toplay20 = tlst[19], toplay21 = tlst[20], toplay22 = tlst[21], toplay23 = tlst[22], songlen = songlength )
 
 ## THE GHOST OF THE SHADOW ##
 
